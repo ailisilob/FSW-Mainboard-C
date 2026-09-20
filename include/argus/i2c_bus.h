35fba2a6c31584c422f4ad4b8654fb5778a6c482
John <jbuttles@andrew.cmu.edu>
2026-09-20 13:26:32 -0400

    Added addr to i2c_device_init.

--- include/argus/i2c_bus.h
+++ include/argus/i2c_bus.h
@@ -55,6 +55,8 @@ typedef struct {
     i2c_inst_t *i2c; // either i2c0 or i2c1
     uint32_t baudrate; // returned by i2c_init (SDK)
     uint32_t timeout; // defaults to I2C_timeout
+    uint sda;
+    uint scl;
     bool init;
     mutex_t lock;
 } i2c_bus_t;
@@ -77,7 +79,7 @@ i2c_status_t i2c_set_baud(i2c_bus_t *bus);
 i2c_status_t i2c_set_mode(i2c_bus_t *bus);
 
 /* Device operations */
-i2c_status_t i2c_device_init(i2c_device_t *dev, i2c_bus_t *bus);
+i2c_status_t i2c_device_init(i2c_device_t *dev, i2c_bus_t *bus, uint8_t addr);
 i2c_status_t i2c_read(i2c_device_t *dev, uint8_t *dst, size_t len);
 i2c_status_t i2c_write(i2c_device_t *dev, const uint8_t *src, size_t len);
 i2c_status_t ic2_write_read(i2c_device_t *dev, const uint8_t *src, uint8_t *dst, size_t len);
