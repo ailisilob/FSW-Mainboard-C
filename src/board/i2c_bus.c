0ca3d728d8a05f4f32ab56b6143c70c4b9119021
John <jbuttles@andrew.cmu.edu>
2026-09-21 12:28:57 -0400

    Implemented scan bus helper function. Passing 50/52.

--- src/board/i2c_bus.c
+++ src/board/i2c_bus.c
@@ -216,5 +216,36 @@ bool reserved_addr(uint8_t addr) {
  *  the SDK I2C example under hardware_i2c.
 */
 i2c_status_t scan_bus(i2c_bus_t *bus) {
-    return i2c_busy;
+    /* Check for non-existent bus and other errors */
+    if (bus == NULL) {return i2c_arg_err;}
+    if (!bus->init) {return i2c_bus_not_init;}
+
+    // Enable UART so we can print status output
+    stdio_init_all();
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(bus->i2c, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
+    return i2c_ok;
 }
